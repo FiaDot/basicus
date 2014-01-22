63361302adde6d4cfd15259d168307624698f2c8
fiadot <fiadot@gmail.com>
2014-01-22 18:32:37 +0900

    issue #2

--- board/models.py
+++ board/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    Title = models.CharField(maxlength=40, null=False)
+
+
+class Article(models.Model):
+    Title = models.CharField(max_length=80)
+    Body = models.TextField(null=False)
+    Created = models.DateTimeField(auto_now_add=True)
+    Updated = models.DateTimeField(auto_now=True)
+    Category = models.ForeignKey(Categories)
+    Comments = models.PositiveIntegerField(default=0, null=True) 
+
+
+class Comments(models.Model):
+    Name = models.CharField(max_length=20, null=False)
+    Password = models.CharField(max_length=32, null=False)
+    Content = models.TextField(max_length=2000, null=False)
+    Created = models.DateTimeField(auto_now_add=True, auto_now=True)
