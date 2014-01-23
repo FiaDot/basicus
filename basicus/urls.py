60b40f88c45576fb09845f352cbf22779040ab6c
fiadot <fiadot@gmail.com>
2014-01-23 16:55:20 +0900

    close #8 URL 추가

--- basicus/urls.py
+++ basicus/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^board/$', 'board.views.board'),
+    url(r'^board/(\d+)/$', 'board.views.article'),
+    url(r'^board/write/$', 'board.views.write'),
+    url(r'^board/comment/(\d+)/$', 'board.views.comment'),    
 )
